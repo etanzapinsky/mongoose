efa5816d1491ca9f60f84fadd6c73dd277c5d163
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-03-19 00:42:15 -0400

    resolved string/name problem

--- simple_lex.py
+++ simple_lex.py
@@ -20,6 +20,9 @@ class Lexer:
 
     # parser was confusing NAME and STRING when no underscore infront of name
     # not sure why, have to look into it
+    # (etan) i think i know what the issue is, name and strign should be the same
+    # token, the meaning, i.e. is one a function name, variable name or string
+    # is derived from the symbols surronding it.
     t_NAME = r'_[a-zA-Z_]+'
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
