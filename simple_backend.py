760f02b81601247ef7871896a028fc10fef7ae05
chrishaum <chrishaum@gmail.com>
2013-03-19 14:19:21 -0400

    rm-ed some files

--- simple_backend.py
+++
@@ -1,5 +0,0 @@
-def walk_ast(root):
-    # this is obviously not extentable since the content of an arg for a function
-    # can be an arbitrary expression, this is just to get something working
-    if root.vtype == 'FUNCTION':
-        root.value(*[child.value for child in root.children])
