aaa56dfa3918460a121984dd24f76fbf5b66c124
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-08 17:03:07 -0400

    recursive def of __eq__

--- parser/base.py
+++ parser/base.py
@@ -7,11 +7,11 @@ tokens = lexer.tokens
 def p_function(p):
     'expr : NAME LPAREN expr RPAREN'
     func = lexer.symbol_table.get(p[1])
-    p[0] = Node(vtype='FUNCTION', value=func, children=[p[3]])
+    p[0] = Node(vtype='FUNCTION', syn_value=func, children=[p[3]])
 
 def p_expr_string(p):
     'expr : QUOTE STRING QUOTE'
-    p[0] = Node(vtype='STRING', value=p[2], children=[])
+    p[0] = Node(vtype='STRING', syn_value=p[2])
 
 # Error rule for syntax errors
 def p_error(p):
--- parser/tree.py
+++ parser/tree.py
@@ -1,23 +1,33 @@
 class Node:
-    def __init__(self, vtype, value, children):
+    def __init__(self, vtype, inh_value=None, syn_value=None, children=[]):
         """
         @param vtype: str
         @param value: <anything>
         @param children: list(Node)
         """
         self.vtype = vtype
-        self.value = value
+        self.inh_value = inh_value
+        self.syn_value = syn_value
         self.children = children
+    
 
     # Useful for testing 
     def __eq__(self, other):
-        return bool(self.vtype == other.vtype and
-                    self.value == other.value and
-                    self.children == other.children) # shallow
+        if self.children == None:
+            return bool(self.vtype == other.vtype and
+                        self.inh_value == other.inh_value and
+                        self.syn_value == other.syn_value)
+        else:
+            self_comp = bool(self.vtype == other.vtype and
+                        self.inh_value == other.inh_value and
+                        self.syn_value == other.syn_value)
+            return self_comp and all([self_c == other_c for self_c, other_c in
+                                  zip(sorted(self.children), sorted(other.children))])
 
     # Useful for debugging
     def __str__(self):
-        return '[Node: {vtype}, {val}, {kids}]'.format(vtype=self.vtype,
-                                                        val=self.value,
-                                                        kids=self.children)
+        return '[Node: {vtype}, {inh_val}, {syn_val}, {kids}]'.format(vtype=self.vtype,
+                                                                      inh_val=self.inh_value,
+                                                                      syn_val=self.syn_value,
+                                                                      kids=self.children)
 
