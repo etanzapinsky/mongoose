f6b940d9c13e27182a190f97569c7aca78e9799b
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-10 21:26:44 -0400

    fixed some minor bugs in manage.sh

--- manage.sh
+++ manage.sh
@@ -1,21 +1,21 @@
 if [ $# -eq 0 ]
 then
-  which bpython >/dev/null 2>&1
-  bpython_status=$?
-  which ipython >/dev/null 2>&1
-  ipython_status=$?
-  if [ $bpython_status -eq 0 ]
-  then
-    PYTHONPATH='.' bpython $*
-  elif [ $ipython_status -eq 0 ]
-  then
-    PYTHONPATH='.' ipython
-  else
-    PYTHONPATH='.' python $*
-  fi
-elif [ $1 = 'test' ]
+    which bpython >/dev/null 2>&1
+    bpython_status=$?
+    which ipython >/dev/null 2>&1
+    ipython_status=$?
+    if [ $bpython_status -eq 0 ]
+    then
+        PYTHONPATH='.' bpython $*
+    elif [ $ipython_status -eq 0 ]
+    then
+        PYTHONPATH='.' ipython $*
+    else
+        PYTHONPATH='.' python $*
+    fi
+elif [ $1 = 'nosetests' ]
 then
-  PYTHONPATH='.' nosetests
+    PYTHONPATH='.' nosetests
 else
-  PYTHONPATH='.' python $*
+    PYTHONPATH='.' python $*
 fi
