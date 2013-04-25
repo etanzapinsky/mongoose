80ae51315303cb9ba946b1444885f0aa8c8489a2
chrishaum <chrishaum@gmail.com>
2013-04-25 19:34:52 -0400

    lots of changes to test_functions

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,7 +2,7 @@ import unittest
 import vtypes as v
 from nose.tools import *
 from parser import Node, Function
-from backend import Backend
+from backend import backend
 from backend.stdlib import first_order_ops
 
 class FunctionsTests(unittest.TestCase):
@@ -22,29 +22,45 @@ class FunctionsTests(unittest.TestCase):
         self.int_node_add = Node(vtype=v.ADD, children=(self.x, self.y))
         self.assignment_node = Node(vtype=v.ASSIGNMENT, children=[self.z, self.int_node_add])
 
+        # return z
+        self.z_statement = Node(vtype=v.STATEMENT_NEWLINE, children=[self.z,])
+        self.return_node = Node(vtype=v.RETURN_STATEMENT, syn_value=self.z_statement)
+
         # Functions
         self.sum_function = Function(return_type=v.INTEGER_VALUE,
+                                     symbol='sum',
                                      parameter_pairs=((self.x, v.INTEGER_VALUE),
                                                       (self.y, v.INTEGER_VALUE)),
-                                     expressions=[self.assignment_node,])
+                                     statements=[self.assignment_node])
 
-        self.backend = Backend()
+        self.sum_with_return_function = Function(return_type=v.INTEGER_VALUE,
+                                                 symbol='sum_with_return',
+                                                 parameter_pairs=((self.x, v.INTEGER_VALUE),
+                                                               (self.y, v.INTEGER_VALUE)),
+                                                 statements=[self.assignment_node,
+                                                          self.return_node,
+                                                          ])
         
 
     def test_bind_params(self):
-        self.sum_function._bind_params(self.int_node_one, self.int_node_two)
-        assert self.sum_function.bindings['x'] == self.int_node_one
+        local_scope = self.sum_function._bind_params(self.int_node_one, self.int_node_two)
+        assert local_scope['x'] == self.int_node_one
+        assert local_scope['y'] == self.int_node_two
 
     def test_scopeless_add(self):
         scopeless_add_node = Node(vtype=v.ADD, children=(self.int_node_one, self.int_node_two))
         scopeless_add_function = Function(return_type=v.INTEGER_VALUE,
-                                               parameter_pairs=(),
-                                               expressions=[scopeless_add_node,])
+                                           symbol="add",
+                                           parameter_pairs=(),
+                                           statements=[scopeless_add_node,])
         scopeless_add_function.execute()
 
-    @nottest
-    def test_execute_statement_without_scope(self):
+    def test_execute_sum_function_no_return(self):
         self.sum_function.execute(self.int_node_one, self.int_node_two)
-        assert self.scope
-        assert self.sum_function.bindings['z'] == self.int_node_three
         # assert self.sum_function.syn_value == self.int_node_three
+
+    def test_execute_sum_with_return_function(self):
+        self.sum_with_return_function.execute(self.int_node_one, self.int_node_two)
+        print self.sum_with_return_function.syn_value
+        assert self.sum_with_return_function.syn_value == 3
+        assert self.int_node_three.syn_value == 3
