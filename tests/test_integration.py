66034ddff95d0497cafa68bdfe8794ffca6709bc
chrishaum <chrishaum@gmail.com>
2013-05-10 13:47:14 -0400

    added integration tests for each of the currently "working" mongoose programs
    Two are failing (run the tests to see).
    ./manage.py tests
    @etanzapinsky

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,7 +7,7 @@ def run(path):
     print output
     return output
 
-def test_declarations_assignment():
+def test_declarations_assignment_mon():
     expected = '''7
 3
 3
@@ -16,9 +16,34 @@ def test_declarations_assignment():
     assert output == expected
 
 
-@nottest
-def test_print():
-    expected = '''
-    '''
+def test_print_mon():
+    expected = '''hello world!
+x: 10
+y: 10
+sum: 20
+x + y: 20
+'''
     output = run('sample_code/working/print.mon')
+    assert output == expected
+
+def test_list_declaration_and_access_mon():
+    expected = '''1
+'''
+    output = run('sample_code/working/list_declaration_and_access.mon')
+    assert output == expected
+
+def test_simple_assignment_mon():
+    expected = '''3
+4
+'''
+    output = run('sample_code/working/simple_assignment.mon')
+    assert output == expected
+
+def test_simple_function_call_mon():
+    expected = '''20
+40
+105
+300
+'''
+    output = run('sample_code/working/simple_function_call.mon')
     assert output == expected
