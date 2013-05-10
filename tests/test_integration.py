405406e7d829dbc3fbbd7b205012b665ac39774e
chrishaum <chrishaum@gmail.com>
2013-05-09 21:53:44 -0400

    added test_integration.py
    This is for integration testing. It runs through the whole stack, like
    a user would from the command line.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,22 @@
 from nose.tools import *
 import subprocess
 
-def run_code_file(path):
-    cmd = ['./manage.sh', 'mongoose.py', path]
-    assert subprocess.call(cmd)
+def run(path):
+    cmd = ['sh', 'manage.sh', 'mongoose.py', path]
+    output = subprocess.check_output(cmd)
+    print output
+    return output
+
+def test_declarations_assignment():
+    expected = '''7
+    3'''
+    output = run('sample_code/working/declarations_assignments.mon')
+    assert output == expected
+
+
+@nottest
+def test_print():
+    expected = '''
+    '''
+    output = run('sample_code/working/print.mon')
+    assert output == expected
